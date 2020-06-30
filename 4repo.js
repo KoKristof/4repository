a666cd279b4a1a072670a32facee3433cb98ca97
Kokristof <kovacskristofattila@gmail.com>
2020-07-01 00:37:58 +0200

    neww

--- 4repo.js
+++ 4repo.js
@@ -3,10 +3,52 @@
   
    $("#myform").submit(function(event){
      event.preventDefault(); var textinp=$("#myinput").val();
-     $("#apple").html(textinp);
+     $("li:empty").first().html(textinp);
+(function(event){ event.preventDefault();
+     $("addd").on("click", (function(event){ event.preventDefault();
+     var r=$('<input type="button"value="Delete"/>');
+     
+     $("li").first().append(r)})
+    )
+ 
    });
+   
+
+   
+    notworking
+   }
+   
+   
+   )
   
-  })
+
+  
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 
 
