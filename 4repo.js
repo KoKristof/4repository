b5ebf4cef7c182842734903e983ef32eb9e47418
Kokristof <kovacskristofattila@gmail.com>
2020-07-02 20:18:02 +0200

    nwe

--- 4repo.js
+++ 4repo.js
@@ -1,28 +1,17 @@
 
   $(document).ready(function(){
+    $("#myform").submit(function(event){
+      event.preventDefault(); var textinp=$("#myinput").val(); 
+      
+      $("#li1").append(textinp) ;
+ 
+    
+    })
   
-   $("#myform").submit(function(event){
-     event.preventDefault(); var textinp=$("#myinput").val(); 
-     
-     $("li:empty").first().html(textinp) ;
-
-   
-
-   
-   })
-   
-  (function orange (){
-     if($("li").is (":empty")){$("li").append(r);
-let r  = $('<input type="button"value="Delete"/>') 
-     }
-   })
-
-   
-
-
-
-
-
+    $("#myform").submit(function(event){
+      event.preventDefault(); var textinp=$("#myinput").val(); 
+      
+      $("#li2").append(textinp) ;})
 
    }
    )
