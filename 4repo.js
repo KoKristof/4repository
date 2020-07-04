e3bed2e2c38ac5b2e3509c481e919d46afc04ca9
Kokristof <kovacskristofattila@gmail.com>
2020-07-04 13:42:10 +0200

    Update 4repo.js

--- 4repo.js
+++ 4repo.js
@@ -9,12 +9,13 @@
     })
   
     $("#myform").submit(function(event){
-      event.preventDefault(); var textinp=$("#myinput").val(); 
+      event.preventDefault(); var textinp=$("#myinput").val();
       
       $("#li2").append(textinp) ;})
 
    }
    )
+   idea: if item exists in before container(li1) {then li2 is good to go }+need callback function
   
    
     
